480975eda36d2d580464fadea0e0b8025d16b44a
David Bouhaben <davidbouhaben@yahoo.co.id>
2026-01-12 21:26:59 +0800

    fix: correct console log typo and clean up whitespace in app.js

--- public/app.js
+++ public/app.js
@@ -1,25 +1,25 @@
 // IMPORT PACKAGES
 // Here you should import the required packages for your Express app: `express` and `morgan`
-
-
+const express = require("express");
+const morganLogger = require("morgan");
 
 // CREATE EXPRESS APP
 // Here you should create your Express app:
-
-
+const app = express();
 
 // MIDDLEWARE
 // Here you should set up the required middleware:
 // - `express.static()` to serve static files from the `public` folder
 // - `express.json()` to parse incoming requests with JSON payloads
 // - `morgan` logger to log all incoming requests
-
-
+app.use(morganLogger("dev"));
+app.use(express.static("public"));
+app.use(express.json())
 
 // ROUTES
 // Start defining your routes here:
 
-
-
 // START THE SERVER
 // Make your Express server listen on port 5005:
+const port = 5005;
+app.listen(port, () => console.logg("The server is istening to port : ", port));
